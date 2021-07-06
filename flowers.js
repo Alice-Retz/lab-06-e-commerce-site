85a5a4d70ea3c6866a0852e60e41b791a0285492
Alice-Retz <alicegretz@gmail.com>
2021-07-06 16:50:09 -0500

    Product data entered.

--- flowers.js
+++ flowers.js
@@ -1,12 +1,51 @@
-export default flowers;
-// const flower = {
-//     id: '',
-//     name: '',
-//     image: '',
-//     meaning: '',
-//     category: '',
-//     price: '', 
-// }
+   
+const petunias = {
+    id: 'petunias',
+    name: 'Petunias',
+    image: 'petunia.jpg',
+    meaning: 'I hate you so much I paid to send you these flowers in order to tell you',
+    category: 'bouquet base',
+    price: '$5.99', 
+};
+const tansies = {
+    id: 'tansies',
+    name: 'Tansies',
+    image: 'tansies.jpg',
+    meaning: `Not only do I not like you, but I'm willing to escalate it to violence.`,
+    category: 'bouquet filler',
+    price: '$1.99', 
+};
+const cyclamens = {
+    id: 'cyclamens',
+    name: 'Cyclamens',
+    image: 'cyclamens.jp',
+    meaning: `We are breaking up, and it's you, not me. `,
+    category: 'potted plant',
+    price: '$6.99', 
+};
+const yCarnations = {
+    id: 'yellow-carnations',
+    name: 'Yellow Carnations',
+    image: 'ycarnations.jpg',
+    meaning: `Forget 'just friends', we're not even that.`,
+    category: 'bouquet base',
+    price: '$5.99', 
+};
+const butterflyWeed = {
+    id: 'butterfly-weed',
+    name: 'Butterfly Weed',
+    image: 'butterflyweed.jpg',
+    meaning: `I'm currently filing for a restraining order.`,
+    category: 'bouquet add-on',
+    price: '$1.99', 
+};
+    
 const flowers = [
-
+    petunias,
+    tansies,
+    cyclamens,
+    yCarnations,
+    butterflyWeed
 ];
+    
+export default flowers;
