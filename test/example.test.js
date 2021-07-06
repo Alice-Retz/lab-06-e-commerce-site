929eacbb45be28ed69ffeab6fd932fdd3f9945f1
Alice-Retz <alicegretz@gmail.com>
2021-07-06 17:44:46 -0500

    TD test passed.

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,21 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { renderFlowers } from '../renderFlowers.js';
+// import petunias from '../flowers.js';
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
+test('will it give me a list item of petunias?', (expect) => {
+    const petuniasLi = `<li><h3>Petunias</h3><img src="./assets/petunia.jpg" alt="Petunias"><span>Meaning: I hate you so much I paid to send you these flowers in order to tell you.</span><span>Bouquet base, $5.99</span><button>Add to Cart</button></li>`;
+    const petunias = {
+        id: 'petunias',
+        name: 'Petunias',
+        image: 'petunia.jpg',
+        meaning: 'I hate you so much I paid to send you these flowers in order to tell you.',
+        category: 'Bouquet base',
+        price: '$5.99', 
+    };
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    const actual = renderFlowers(petunias);
+
+    expect.equal(actual.outerHTML, petuniasLi);
 });
