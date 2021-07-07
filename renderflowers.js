33bdaefae291fe00055b3685e0e9f67453e4e828
Alice-Retz <alicegretz@gmail.com>
2021-07-07 09:29:22 -0500

    New styling added.

--- renderflowers.js
+++ renderflowers.js
@@ -1,5 +1,7 @@
 export function renderFlowers(flowers){
     const flowerLI = document.createElement('li');
+    flowerLI.classList.add('flowerItem');
+
     const flowerName = document.createElement('h3');
     flowerName.textContent = flowers.name;
     
@@ -9,9 +11,11 @@ export function renderFlowers(flowers){
     
     const flowerMean = document.createElement('span');
     flowerMean.textContent = `Meaning: ${flowers.meaning}`;
+    flowerMean.classList.add('productDescription');
 
     const flowerSpan = document.createElement('span');
     flowerSpan.textContent = `${flowers.category}, ${flowers.price}`;
+    flowerSpan.classList.add('productPrice');
     
     const button = document.createElement('button');
     button.textContent = 'Add to Cart';
