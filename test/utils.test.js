5f93259998efa95f8bb32a7d35500b8bbd3beb43
Alice-Retz <alicegretz@gmail.com>
2021-07-07 15:47:14 -0500

    Functions written, ready for testing.

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,18 +1,4 @@
-import 
+import { findById, calcItemTotal, renderTableRow } from '../utils.js'
+import flowers from '../data/flowers.js'
 
-test ('renderTableRow returns a <tr> element', expect =>
-const petunia = {
-    id: '1',
-    name: 'Petunias',
-    image: 'petunia.jpg',
-    meaning: 'I hate you so much I paid to send you these flowers in order to tell you.',
-    category: 'Bouquet base',
-    price: '$5.99',
-};
-const petuniaCart = {
-    id: 1,
-    qty: 2,
-}
-const expected = '';
-const dome = renderTableRow(petunia, petuniaCart)
-)
+const test = Qunit.test;
--- utils.js
+++ utils.js
@@ -1,22 +1,43 @@
 export function findById(items, id){
     for (const item of items){
         if (item.id === id) {
-        return item;
+            return item;
         }
     }
 }
 
-export function findById(){
+export function calcItemTotal(flowers, cart){
+    let orderTotal = 0;
+    for (let item of cart) {
+        const flower = findById(flowers, item.id);
+        orderTotal += flower.price * item.qty;
+    }
+    return orderTotal;
+}
 
+export function toUSD(number) {
+    return number.toLocalString(
+        'en-US', { style: 'currency', currency: 'USD' }
+    );
 }
 
 export function renderTableRow(cartItem, flowerItem){
     const tr = document.createElement('tr');
     const tdName = document.createElement('td');
-    tdName.textContent = cartItem.name;
+    tdName.textContent = flowerItem.name;
     tr.appendChild(tdName);
 
     const tdPrice = document.createElement('td');
-    tdPrice.textContent = cartItem.price;
-    
+    tdPrice.textContent = flowerItem.price;
+    tr.appendChild(tdPrice);
+
+    const tdQty = document.createElement('td');
+    tdQty.textContent = cartItem.qty;
+    tr.appendChild(tdQty);
+
+    const tdTotal = document.createElement('td');
+    const total = flowerItem.price * cartItem.qty;
+    tdTotal.textContent = toUSD(total);
+
+    return tr;
 }
