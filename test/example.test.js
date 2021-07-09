96563bade801e75526fb8d12d92c043c5a714a3b
Alice-Retz <alicegretz@gmail.com>
2021-07-09 11:23:09 -0500

    Fixed failing DOM test.

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,6 @@
 import { findById, calcItemTotal, renderTableRow } from '../utils.js';
 import flowers from '../data/flowers.js';
+import { renderFlowers } from '../renderflowers.js';
 
 const test = QUnit.test;
 
@@ -60,19 +61,19 @@ test('does it render the tables properly', expect => {
 
 
 
-// Day 1 test
-// test('will it give me a list item of petunias?', (expect) => {
-//     const petuniasLi = `<li><h3>Petunias</h3><img src="./assets/petunia.jpg" alt="Petunias"><span>Meaning: I hate you so much I paid to send you these flowers in order to tell you.</span><span>Bouquet base, $5.99</span><button>Add to Cart</button></li>`;
-//     const petunias = {
-//         id: 'petunias',
-//         name: 'Petunias',
-//         image: 'petunia.jpg',
-//         meaning: 'I hate you so much I paid to send you these flowers in order to tell you.',
-//         category: 'Bouquet base',
-//         price: '$5.99', 
-//     };
 
-//     const actual = renderFlowers(petunias);
+test('will it give me a list item of petunias?', (expect) => {
+    const petuniasLi = `<li class=\"flowerItem\"><h3>Petunias</h3><img src=\"./assets/petunia.jpg\" alt=\"Petunias\"><span class=\"productDescription\">Meaning: I hate you so much I paid to send you these flowers in order to tell you.</span><span class=\"productPrice\">Bouquet base, $6.00</span><button value=\"petunias\" class=\"add\">Add to Cart</button></li>`;
+    const petunias = {
+        id: 'petunias',
+        name: 'Petunias',
+        image: 'petunia.jpg',
+        meaning: 'I hate you so much I paid to send you these flowers in order to tell you.',
+        category: 'Bouquet base',
+        price: '6', 
+    };
 
-//     expect.equal(actual.outerHTML, petuniasLi);
-// });
+    const actual = renderFlowers(petunias);
+
+    expect.equal(actual.outerHTML, petuniasLi);
+});
